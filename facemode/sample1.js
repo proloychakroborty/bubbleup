a9db1d6289f1f24814645f21fffb0ab5d4482092
Proloy Chakroborty <proloy.chakroborty@harman.com>
2018-03-26 16:16:17 +0530

    DetectRTC

--- facemode/sample1.js
+++ facemode/sample1.js
@@ -1,4 +1,4 @@
-/*global navigator, console, document, window */
+/*global navigator, console, document, window, DetectRTC */
 /*jslint es5: true */
 /*eslint no-console: ["error", { allow: ["log"] }] */
 
@@ -6,57 +6,114 @@
     'use strict';
     window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
 
-    navigator.mediaDevices
-        .getUserMedia({
-            video: {
-                facingMode: {
-                    exact: "user"
-                }
-            },
-            audio: false
-        })
-        .then(function (stream) {
-            document.getElementById('type').innerHTML = 'First Try - facingMode: user';
-            var video = document.createElement('video');
-            video.muted = true;
-            video.src = window.URL.createObjectURL(stream);
-            (document.body || document.documentElement).appendChild(video);
-        })
-        .catch(function (error1) {
-            console.log('MediaStream Error 1', error1);
-            navigator.mediaDevices
-                .getUserMedia({
-                    video: {
-                        facingMode: {
-                            exact: "environment"
-                        }
-                    },
-                    audio: false
-                })
-                .then(function (stream) {
-                    document.getElementById('type').innerHTML = 'Second Try - facingMode: environment';
-                    var video = document.createElement('video');
-                    video.muted = true;
-                    video.src = window.URL.createObjectURL(stream);
-                    (document.body || document.documentElement).appendChild(video);
-                })
-                .catch(function (error2) {
-                    console.log('MediaStream Error 2', error2);
-                    navigator.mediaDevices
-                        .getUserMedia({
-                            video: true,
-                            audio: false
-                        })
-                        .then(function (stream) {
-                            document.getElementById('type').innerHTML = 'Third Try - no facingMode';
-                            var video = document.createElement('video');
-                            video.muted = true;
-                            video.src = window.URL.createObjectURL(stream);
-                            (document.body || document.documentElement).appendChild(video);
-                        })
-                        .catch(function (error3) {
-                            console.log('MediaStream Error 3', error3);
-                        });
+
+    DetectRTC.load(function () {
+        navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: true
+        }).then(function (stream) {
+            var video;
+            try {
+                video = document.createElement('video');
+                video.muted = true;
+                video.src = window.URL.createObjectURL(stream);
+                video.style.display = 'none';
+                (document.body || document.documentElement).appendChild(video);
+            } catch (e) {
+                console.log(e);
+            }
+
+            DetectRTC.load(function () {
+                var selectDevice = document.getElementById('webcam');
+                DetectRTC.videoInputDevices.forEach(function (val) {
+                    var option = document.createElement('option');
+                    option.value = val.deviceId;
+                    option.text = val.label;
+                    selectDevice.appendChild(option);
                 });
+
+                // release camera
+                stream.getTracks().forEach(function (track) {
+                    track.stop();
+                });
+
+                if (video && video.parentNode) {
+                    video.parentNode.removeChild(video);
+                }
+            });
         });
+    });
+
+    function onWebcam() {
+        var selectDevice = document.getElementById('webcam'),
+            device = selectDevice.value,
+            mediaConstraint = {};
+
+        if (device) {
+            mediaConstraint = {
+                video: {
+                    deviceId: {
+                        exact: device
+                    },
+                    facingMode: {
+                        exact: "user"
+                    }
+                },
+                audio: false
+            };
+        } else {
+            mediaConstraint = {
+                video: {
+                    facingMode: {
+                        exact: "user"
+                    }
+                },
+                audio: false
+            };
+        }
+
+        navigator.mediaDevices
+            .getUserMedia(mediaConstraint)
+            .then(function (stream) {
+                document.getElementById('type').innerHTML = 'First Try - facingMode: user';
+                var video = document.createElement('video');
+                video.muted = true;
+                video.src = window.URL.createObjectURL(stream);
+                (document.body || document.documentElement).appendChild(video);
+            })
+            .catch(function (error1) {
+                console.log('MediaStream Error 1', error1);
+                mediaConstraint.video.facingMode.exact = "environment";
+                navigator.mediaDevices
+                    .getUserMedia(mediaConstraint)
+                    .then(function (stream) {
+                        document.getElementById('type').innerHTML = 'Second Try - facingMode: environment';
+                        var video = document.createElement('video');
+                        video.muted = true;
+                        video.src = window.URL.createObjectURL(stream);
+                        (document.body || document.documentElement).appendChild(video);
+                    })
+                    .catch(function (error2) {
+                        console.log('MediaStream Error 2', error2);
+                        mediaConstraint.video = true;
+                        navigator.mediaDevices
+                            .getUserMedia(mediaConstraint)
+                            .then(function (stream) {
+                                document.getElementById('type').innerHTML = 'Third Try - no facingMode';
+                                var video = document.createElement('video');
+                                video.muted = true;
+                                video.src = window.URL.createObjectURL(stream);
+                                (document.body || document.documentElement).appendChild(video);
+                            })
+                            .catch(function (error3) {
+                                console.log('MediaStream Error 3', error3);
+                            });
+                    });
+            });
+    }
+
+    onWebcam();
+
+    var selectDevice = document.getElementById('webcam');
+    selectDevice.onchange = onWebcam;
 }());
